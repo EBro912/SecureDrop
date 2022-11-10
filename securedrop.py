6bfe3c5cccb25919326d65bcd132570bd7ea90a3
Alex <alexsheehan02@gmail.com>
2022-11-10 10:17:54 -0500

    Functions Added     Encrypt Function     Decrypt Function     retrievePassword Function     retrieveSalt Function
    Functionality Changed
        user.json now stores paremeter Salt along with user, email, password
    
    Bugs Fixed
        Error with
            if os.path.exists("contacts.json") is False or os.path.getsize("contacts.json") is 0
        to
            if os.path.exists("contacts.json") is False or os.path.getsize("contacts.json") == 0

--- securedrop.py
+++ securedrop.py
@@ -2,7 +2,15 @@ import json
 import os.path
 import pwinput
 import bcrypt
-
+import base64
+# For Encoding / Decoding the salt
+from base64 import b64encode, b64decode
+# For Encrpyting / Decrpyting
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+# For urandom
+import os
 #-------------------------------------------------
 #
 # libraries used:
@@ -17,6 +25,65 @@ class User:
         self.name = name
         self.email = email
 
+
+
+# Usage - Return password from json in memory
+def retrievePassword():
+    data = json.loads(open("user.json", "r").read())
+    return data["password"]
+
+# Usage - Return salt from json in memory
+def retrieveSalt():
+    data = json.loads(open("user.json", "r").read())
+    hold = b64decode(data["salt"])
+    return hold
+
+
+"""
+    Usage: Takes in data variable non encoded and encrypts data
+    Return Value: returns encrypted data non byte encoded
+    Notes: This can be used when reading from json file, to encrypt that data then return json writeable data
+"""
+def Encrypt(data):
+    #Encrypt data passed
+    data = data.encode('utf-8')
+    #password = get password
+    password = retrievePassword().encode('utf-8')
+    #Retreive salt
+    salt = retrieveSalt()
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=390000
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    token = f.encrypt(data)
+    return token.decode('utf-8')
+
+
+"""
+    Usage: Takes in data variable non encoded and decrypts data
+    Return Value: returns decrypted data non byte encoded
+    Notes: This can be used when reading from json file, to decrypt that data then return readable data
+"""
+def Decrypt(data):
+    data = data.encode('utf-8')
+    #password = get password
+    password = retrievePassword().encode('utf-8')
+    #Retreive salt
+    salt = retrieveSalt()
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=390000
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    token = f.decrypt(data)
+    return token.decode('utf-8')
+
+
 def handleHelp():
     print('  "add" -> Add a new contact')
     print('  "list" -> List all online contacts')
@@ -26,6 +93,9 @@ def handleHelp():
 def handleAdd():
     name = input('Enter Full Name: ')
     email = input('Enter Email Address: ')
+    # Encrypting name / email, uncomment when list / add feauture figured out
+    # name = Encrypt(name)
+    # email = Encrypt(email)
     global contact_cache
     # ensure the user doesn't already exist
     if contact_cache is not None:
@@ -53,12 +123,14 @@ def handleSend():
     print("TODO")
 
 def loadContacts():
-    if os.path.exists("contacts.json") is False or os.path.getsize("contacts.json") is 0:
+    if os.path.exists("contacts.json") is False or os.path.getsize("contacts.json") == 0:
         return []
     # TODO: decrypt file here after encryption
     data = json.loads(open("contacts.json", "r").read())
     contacts = []
     for user in data:
+        # Decryption of contacts, replace line below once the list, add function is figured out
+        # contact = {"name":Decrypt(user['name']), "email":Decrypt(user['email'])}
         contact = {"name":user['name'], "email":user['email']}
         contacts.append(contact)
     return contacts
@@ -81,9 +153,13 @@ def createUser():
     else:
         print("Passwords Match.")
         enc_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-        # TODO: encrypt the entire file besides just the password, similar to Task 3
+        # This is the salt used individually for each user, it is okay to store it in plaintext as it is only
+        # used to stop rainbow table attacks
+        salt = os.urandom(16)
+        # Makes it storable in json,its random bytes so cant just use decode
+        salt = b64encode(salt).decode('utf-8')
         with open("user.json", "w") as output:
-            output.write(json.dumps({"name": name, "email": email, "password": enc_pass}))
+            output.write(json.dumps({"name": name, "email": email, "password": enc_pass, "salt": salt}))
         print("User Registered.")
 
 def loadUser():
